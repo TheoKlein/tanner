e440974fc471b7051cdc1a34d6e580ac605b86a3
Theo <theokleintw@gmail.com>
2020-10-10 15:07:41 +0800

    escape post data

--- tanner/reporting/log_local.py
+++ tanner/reporting/log_local.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime
 from tanner import config
-
+from urllib.parse import urlencode
 
 class Reporting:
     @staticmethod
@@ -9,5 +9,7 @@ class Reporting:
         report_file = config.TannerConfig.get('LOCALLOG', 'PATH')
         with open(report_file, 'a') as out:
             session_data["timestamp"] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%f')
+            if session_data.get('post_data', None):
+                session_data['post_data'] = urlencode(session_data['post_data'])
             json.dump(session_data, out)
             out.write('\n')
